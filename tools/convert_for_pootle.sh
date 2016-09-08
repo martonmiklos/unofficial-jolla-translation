d1a1e7aeb8b3a9b845be2b9ca6fd565e57ffb94e
Miklos Marton <martonmiklosqdev@gmail.com>
2016-09-08 19:08:04 +0200

    Updated generator script to generate files to the lang folder not to the templates.

--- tools/convert_for_pootle.sh
+++ tools/convert_for_pootle.sh
@@ -5,7 +5,10 @@ TEMPLATES_DIR="../templates"
 OUTPUT_DIR="../converted_for_pootle"
 TARGET_LANG="hu"
 TRANSLATION_MEMORY_PATH="../transifex_eng/translations"
+TRANSLATION_MEMORY_LANG="en_GB"
+
 retval=0
+
 for d in $TRANSLATIONS_DIR/*/ ; do
     # componentname will be something like: alarm-ui
     componentName=`basename "$d" | sed -E 's/unofficial-jolla-translations\.(.*)/\1/g'`
@@ -14,13 +17,16 @@ for d in $TRANSLATIONS_DIR/*/ ; do
     # there might be multiple files with the same contents
     for templatePath in $(find $TEMPLATES_DIR -type f -name "$componentName.ts"); do
         # outpath will store something like jolla-alarm-ui/templates
-        outpath=`dirname $templatePath | sed -E 's/..\/templates\/(.*)/\1/g'`
+        outpath=`dirname $templatePath | sed -E 's/..\/templates\/(.*)\/templates/\1/g'`
         # create that dir in the output folder
-        mkdir -p $OUTPUT_DIR/$outpath
-        ./transifex_to_pootle_converter.py $d/hu_HU.ts $templatePath $OUTPUT_DIR/$outpath/$componentName.ts $TARGET_LANG $TRANSLATION_MEMORY_PATH/$d/en_GB.ts
+        outpath=$OUTPUT_DIR/$TARGET_LANG/$outpath/$TARGET_LANG
+        echo $outpath
+        mkdir -p $outpath
+        rm -rf $outpath/*
+        ./transifex_to_pootle_converter.py $d/hu_HU.ts $templatePath $outpath/$componentName.ts $TARGET_LANG $TRANSLATION_MEMORY_PATH/$d/$TRANSLATION_MEMORY_LANG.ts
         retval=$?
         if [ $retval -ne 0 ]; then
-            echo "./transifex_to_pootle_converter.py $d/hu_HU.ts $templatePath $OUTPUT_DIR/$outpath/$componentName.ts $TARGET_LANG $TRANSLATION_MEMORY_PATH/$d/en_GB.ts failed"
+            echo "./transifex_to_pootle_converter.py $d/hu_HU.ts $templatePath $outpath/$componentName.ts $TARGET_LANG $TRANSLATION_MEMORY_PATH/$d/$TRANSLATION_MEMORY_LANGts failed"
             break
         fi
     done
